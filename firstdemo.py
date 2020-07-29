b27dc1305b156b28b854dbabaaebe82720561164
viralsir <viralsir2018@gmail.com>
2020-07-29 10:23:28 +0530

    i have added maximum number functionality

--- firstdemo.py
+++ firstdemo.py
@@ -1,6 +1,9 @@
 no1=int(input("Enter No:"))
 no2=int(input("Enter No:"))
 
-print("no1:",no1);
-print("no2:",no2);
+
+if no1>no2:
+    print(no1," is a maximum no");
+else :
+    print(no2," is a maximum no");
 
