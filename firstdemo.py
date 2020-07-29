e2f0c35aa7e004d80ede99355f3b270af9b72eea
viralsir <viralsir2018@gmail.com>
2020-07-29 10:40:10 +0530

    added third number functionality

--- firstdemo.py
+++ firstdemo.py
@@ -1,11 +1,13 @@
 import math;
 no1=int(input("Enter No:"))
 no2=int(input("Enter No:"))
+no3=int(input("Enter No:"))
 
-
-if no1>no2:
+if no1>no2 and no1>no3:
     print(no1," is a maximum no");
-else :
+elif no2>no1 :
     print(no2," is a maximum no");
+else :
+    print(no3," is a maximum no");
 
 print("Factorial :",math.factorial(no1));
